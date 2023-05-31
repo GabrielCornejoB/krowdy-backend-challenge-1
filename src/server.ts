e76ccdc46eea1ef1ab22fab63a6e65f5689440f9
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-31 11:30:58 -0500

    Loggers setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import { createServer } from "restify";
+import { createLogger } from "bunyan";
+import morgan from "morgan";
+import { spawn } from "node:child_process";
 
-const server = createServer();
+const server = createServer({
+  name: "My server",
+  log: createLogger({
+    name: "audit",
+    level: "error",
+  }),
+});
+server.use(morgan("dev"));
 
 const initServer = async () => {
   server.listen(process.env.SERVER_PORT, () => {
-    console.log("Server listening in PORT", server.url);
+    console.log("Server listening in PORT", process.env.SERVER_PORT);
   });
 };
 
