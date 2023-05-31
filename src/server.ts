83c12e5fb687ec493811064935ead5e6cc0a27c8
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-31 11:48:18 -0500

    Endpoint for testing ffmpeg cmd

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,35 @@ const server = createServer({
 });
 server.use(morgan("dev"));
 
+const test = () =>
+  new Promise((resolve, reject) => {
+    const cmd = spawn("ffmpeg", ["-version"]);
+    cmd.stdout.on("data", (data) => {
+      console.log(data);
+    });
+    cmd.stderr.on("error", (data) => {
+      console.log(data);
+    });
+    cmd.on("close", (code) => {
+      resolve("Resolve code -> " + code);
+    });
+  });
+
+server.get("/api/v1/ffmpegtest", async (req, res) => {
+  try {
+    const responseCode = await test();
+    return res.json({
+      message: "Success",
+      code: responseCode,
+    });
+  } catch (error) {
+    return res.json({
+      message: error,
+      error: true,
+    });
+  }
+});
+
 const initServer = async () => {
   server.listen(process.env.SERVER_PORT, () => {
     console.log("Server listening in PORT", process.env.SERVER_PORT);
